95a3fd20b264ab9af9b7c7f0e257c68cc43684e8
Vladimir Syerik <vladimir@syerik.net>
2021-12-17 09:37:02 -0600

    Settings - Application - hide for now. Resolves #15.

--- web/components/settings/Settings.jsx
+++ web/components/settings/Settings.jsx
@@ -6,16 +6,16 @@ import { Link } from 'react-router-dom'
 export default function Settings () {
   return <div className='dataContainer' style={{ padding: '10px' }}>
     <div className='row'>
-      <Card title='Application' text='Application settings' url='app' />
+      {/*<Card title='Application' text='Application settings' url='app' />*/}
       <Card title='Account' text='Setup your financial account' url='accounts' />
       <Card title='Users' text='Application users' url='users' />
       <Card title='Businesses' text='Setup your businesses' url='businesses' />
+      <Card title='Categories' text='Setup transaction categories' url='categories' />
     </div>
     <div className='row'>
-      <Card title='Categories' text='Setup transaction categories' url='categories' />
       <Card title='Cars' text='Cars used for business' url='cars' />
     </div>
-    <div className='row'>
+    <div className='row mt-3'>
       <Card title='Imports' text='Import financial data' url='imports' />
       <Card title='Import Rules' text='Import formats and rules' url='importRules' />
     </div>
