0eb19b5ba4adb50188d5ed2f2b93380fe40e0c1e
Vladimir Syerik <vladimir@syerik.net>
2024-06-10 15:16:30 -0500

    1.10.0

---
+++ typeDef/shared.dashboard.td.js
@@ -0,0 +1,14 @@
+'use strict'
+
+/**
+ * @typedef {Object} BusinessPL
+ * @property {number} year
+ * @property {number} month
+ * @property {string} type
+ * @property {string} type
+ * @property {string} categoryId
+ * @property {string} name
+ * @property {number} total
+ */
+
+export const d = []
